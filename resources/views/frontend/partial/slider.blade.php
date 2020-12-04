929c1dccb6b4b2c8baa19dc4116ff3d76ba2c0fd
Waytoweb <kashyap.waytoweb@gmail.com>
2020-12-04 15:45:56 +0530

    4-12-2020 Code Merger

--- resources/views/frontend/partial/slider.blade.php
+++ resources/views/frontend/partial/slider.blade.php
@@ -49,14 +49,14 @@
                         @foreach($sideBarData as $key=>$value)
                             <div class="carousel-item list-banneroflist {{($key == 0) ? "active" : ""}}">
                                 <img src="{{asset($value['banner_for_web'])}}" alt="...">
-                                <div class="carousel-caption d-none d-md-block ">
+                                <div class="carousel-caption  d-md-block ">
                                     <h5 class="banner-title">{{$value['title']}}</h5>
                                     <p class="banner-title-desc">{!!$value['description']  !!}</p>
                                 </div>
                             </div>
                             <div class="carousel-item list-banneroflist_mobile {{($key == 0) ? "active" : ""}}">
                                 <img src="{{asset($value['banner_for_web'])}}" alt="...">
-                                <div class="carousel-caption d-none d-md-block ">
+                                <div class="carousel-caption  d-md-block ">
                                     <h5 class="banner-title">{{$value['title']}}</h5>
                                     <p class="banner-title-desc">{!!$value['description']  !!}</p>
                                 </div>
