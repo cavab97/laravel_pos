d86ecb57d1089448d84d1abac7e92c3b7774be95
Boon Jun Tan <boonjun@marslab.com.my>
2021-03-02 18:49:28 +0800

    Add server id to customer table

---
+++ database/migrations/2021_03_02_183043_add_server_id_to_customer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddServerIdToOrderDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            $table->integer('server_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            if (Schema::hasColumn('customer', 'server_id'))
+            {
+                $table->dropColumn(['server_id']);
+            }
+        });
+    }
+}
