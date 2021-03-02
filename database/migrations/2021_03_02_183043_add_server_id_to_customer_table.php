c322eac506f02d3fa85cb1183f2d7407383cd32f
Boon Jun Tan <boonjun@marslab.com.my>
2021-03-02 18:51:22 +0800

    Change class name for migrate file

--- database/migrations/2021_03_02_183043_add_server_id_to_customer_table.php
+++ database/migrations/2021_03_02_183043_add_server_id_to_customer_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddServerIdToOrderDetailTable extends Migration
+class AddServerIdToCustomerTable extends Migration
 {
     /**
      * Run the migrations.
